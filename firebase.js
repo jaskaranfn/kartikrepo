88c68542516766389a9cdbadbc19d5dfd97a7f84
jaskaranfn <132913622+jaskaranfn@users.noreply.github.com>
2024-03-30 12:28:05 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,12 @@
 // firebase.js
 // Initialize Firebase app with your Firebase project configuration
 const firebaseConfig = {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_AUTH_DOMAIN',
-    databaseURL: 'YOUR_DATABASE_URL',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_STORAGE_BUCKET',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    appId: 'YOUR_APP_ID'
-  };
-  firebase.initializeApp(firebaseConfig);
-  
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  databaseURL: 'YOUR_DATABASE_URL',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID'
+};
+firebase.initializeApp(firebaseConfig);
