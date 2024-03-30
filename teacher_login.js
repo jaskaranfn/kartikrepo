1ecc8ea5cc1bcb4f4c3f73e40db4d8f4240fa5e8
jaskaranfn <132913622+jaskaranfn@users.noreply.github.com>
2024-03-30 12:26:12 +0530

    teacher_login.js

--- teacher_login.js
+++ teacher_login.js
@@ -1,17 +1,14 @@
 // teacher_login.js
-document.getElementById('teacherLoginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.getElementById('teacherEmail').value;
-    const password = document.getElementById('teacherPassword').value;
-  
-    // Authenticate user using Firebase Authentication
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(() => {
-        console.log('Teacher login successful');
-        window.location.href = 'send_message.html'; // Redirect to message sending page
-      })
-      .catch(error => {
-        console.error('Teacher login failed:', error.message);
-      });
+document.addEventListener('DOMContentLoaded', function () {
+  const googleSignInButton = document.getElementById('googleSignInButton');
+
+  // Render Google sign-in button
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({
+    'prompt': 'select_account'
   });
-  
+
+  googleSignInButton.addEventListener('click', function () {
+    firebase.auth().signInWithRedirect(provider);
+  });
+});
